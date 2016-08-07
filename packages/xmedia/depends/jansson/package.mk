99a17058377052447da4df1708cf43a6180ba3fb
Alexandr <alex@alexelec.in.ua>
2016-08-07 16:53:47 +0300

    vdr-plugin-weatherforecast 0.2.0

---
+++ packages/xmedia/depends/jansson/package.mk
@@ -0,0 +1,19 @@
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
+PKG_NAME="jansson"
+PKG_VERSION="2.7"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.digip.org/jansson/"
+PKG_URL="http://www.digip.org/$PKG_NAME/releases/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="xmedia/depends"
+PKG_SHORTDESC="Jansson is a C library for encoding, decoding and manipulating JSON data"
+PKG_LONGDESC="Jansson is a C library for encoding, decoding and manipulating JSON data"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
