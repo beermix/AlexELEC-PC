d149bf7caf9f2085ded599db83ad634ceabd4a63
Alexandr <alex@alexelec.in.ua>
2016-08-08 18:53:04 +0300

    add: inputlirc-23

--- packages/virtual/xmedia/package.mk
+++ packages/virtual/xmedia/package.mk
@@ -19,7 +19,7 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 # tools
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc scan-s2"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc scan-s2 inputlirc"
 
 # camd
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET wicard oscam"
