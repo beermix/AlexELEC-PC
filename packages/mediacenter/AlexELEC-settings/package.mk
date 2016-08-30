9404a3501a6d90b49f676e482f9ea251d4fa16ea
Alexandr <alex@alexelec.in.ua>
2016-08-30 17:24:53 +0300

    size of system partition 1024Mb; AlexELEC-settings: 8353516

--- packages/mediacenter/AlexELEC-settings/package.mk
+++ packages/mediacenter/AlexELEC-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="AlexELEC-settings"
-PKG_VERSION="31b21b9"
+PKG_VERSION="8353516"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
