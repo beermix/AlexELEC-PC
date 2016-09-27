6b7931393bc2d400ea4cf379dd5185473b4c62d8
Alexandr <alex@alexelec.in.ua>
2016-09-27 20:35:15 +0300

    update AlexELEC-settings

--- packages/mediacenter/AlexELEC-settings/package.mk
+++ packages/mediacenter/AlexELEC-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="AlexELEC-settings"
-PKG_VERSION="34952c0"
+PKG_VERSION="e9991fa"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
