59d9f8962dc02085b9f9638086db764ea6921711
Alexandr <alex@alexelec.in.ua>
2016-10-01 20:06:06 +0300

    AlexELEC-settings: add boblightd

--- packages/mediacenter/AlexELEC-settings/package.mk
+++ packages/mediacenter/AlexELEC-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="AlexELEC-settings"
-PKG_VERSION="c5166e1"
+PKG_VERSION="8a72cbf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
