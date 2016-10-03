f455f02c16a3dd52ace87cf257b8149f433f89f7
Alexandr <alex@alexelec.in.ua>
2016-10-03 19:42:24 +0300

    upd AMD Graphics driver

--- packages/x11/driver/xf86-video-amdgpu/package.mk
+++ packages/x11/driver/xf86-video-amdgpu/package.mk
@@ -1,23 +1,10 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="xf86-video-amdgpu"
-PKG_VERSION="1.1.0"
+PKG_VERSION="1.1.1"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
@@ -28,7 +15,6 @@ PKG_PRIORITY="optional"
 PKG_SECTION="x11/driver"
 PKG_SHORTDESC="xf86-video-amdgpu - AMD Radeon video driver for the Xorg X server"
 PKG_LONGDESC="AMD Xorg video driver"
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
--- packages/x11/driver/xf86-video-ati/package.mk
+++ packages/x11/driver/xf86-video-ati/package.mk
@@ -1,23 +1,10 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="xf86-video-ati"
-PKG_VERSION="7.7.0"
+PKG_VERSION="7.7.1"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
@@ -28,7 +15,6 @@ PKG_PRIORITY="optional"
 PKG_SECTION="x11/driver"
 PKG_SHORTDESC="xf86-video-ati: The Xorg driver for ATI video chips"
 PKG_LONGDESC="The ati driver supports various ATi, know AMD, video chips."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
